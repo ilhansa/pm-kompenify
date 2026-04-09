c49e8ba69b0a8d0c5291302ad285d997016b333a
Yulia Nadzifah <ilhansalihilsa@gmail.com>
2026-04-09 21:58:47 +0700

    merge logout, splash screen

--- prototypekompenify/lib/main.dart
+++ prototypekompenify/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'login.dart';
+import 'splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginPage(), // langsung ke login
+      home: const SplashScreen(), // langsung ke splash screen
     );
   }
 }
