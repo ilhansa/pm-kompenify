d416655266e6c1c2ddf5ee2bef88ddfc5ed079f3
Joeyzeeee <krisna.adstr@gmail.com>
2026-06-03 14:04:46 +0700

    add model PengajuanKompen

---
+++ kompenify-laravel/app/Models/PengajuanKompen.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PengajuanKompen extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pengajuan_kompen';
+
+    protected $fillable = [
+        'id',
+        'mahasiswa_id',
+        'assignment_id',
+        'alasan',
+        'status',
+    ];
+
+    public $incrementing = false; // karena pakai UUID
+    protected $keyType = 'string';
+}
