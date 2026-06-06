417d2cb7ecc777416543304395f1dbc5b9650c3a
Sultan Syarif Ubaidillah Al Nashr <sultansyarif630@gmail.com>
2026-06-06 18:57:58 +0700

    ubah url

--- admin-kompenify/src/api.js
+++ admin-kompenify/src/api.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    // Membaca url dari file .env
+    baseURL: import.meta.env.VITE_API_BASE_URL ??  'http://127.0.0.1:8000/api', 
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
+        // Bypass halaman warning ngrok
+        'ngrok-skip-browser-warning': 'true' 
     },
-    // Memaksa Axios menganggap status 200-300 murni sukses tanpa rewel masalah preflight
     validateStatus: function (status) {
         return status >= 200 && status < 300; 
     }
 });
 
-// Otomatis tempelkan token keamanan di setiap tembakan API
+// Interceptor Token Authorization
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('auth_token');
     if (token) {
